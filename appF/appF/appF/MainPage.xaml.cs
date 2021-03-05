3c681f3d6c0191a11900eb3a1a77dc50c90b9a96
Henry Barrientos Flores <henryflo1389@gmail.com>
2021-03-05 01:15:46 -0600

    P

---
+++ appF/appF/appF/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace appF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            int f = Convert.ToInt32(resultado.Text);
+            int f1 = f - 365;
+            string m = f1.ToString();
+            int a = m.Length;
+            string c = f1.ToString();
+            int b = c.Length;
+            string A = m.Substring(0, 2);
+            string B = c.Substring(2, 2);
+            DisplayAlert("Dia de nacimiento es ", A,"cancelar  ");
+            DisplayAlert("Mes  de nacimiento es ", B, "dia ");
+        }
+    }
+}
---
+++ appN/appN/appN/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace appN
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double resultado = Convert.ToDouble(res.Text);
+            double R = (resultado / 5 - 8) / 2;
+            DisplayAlert("el numero que estas pensando es ", R.ToString(),"salir");
+        }
+    }
+}
